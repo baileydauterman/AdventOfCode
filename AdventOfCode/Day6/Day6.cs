0c425afca9506a84c81d575ff17ef1a001a53050
baileydauterman <dauterman.bailey@gmail.com>
2022-12-06 21:28:25 -0500

    add: day 6

---
+++ AdventOfCode/Day6/Day6.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode
+{
+    internal class Day6
+    {
+
+        public static int FindMarker(string path)
+        {
+            var data = File.ReadAllText(path);
+
+            return FindMarker(data, 4);
+        }
+
+        public static int FindMarkerString(string data)
+        {
+            return FindMarker(data, 4);
+        }
+
+        public static int FindMessage(string path)
+        {
+            var data = File.ReadAllText(path);
+
+            return FindMarker(data, 14);
+        }
+
+        public static int FindMessageString(string data)
+        {
+            return FindMarker(data, 14);
+        }
+
+        private static int FindMarker(string data, int len)
+        {
+            for (var i = 0; i < data.Length - len; i++)
+            {
+                if (data.Substring(i, len).Distinct().Count() == len)
+                {
+                    return i + len;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -32,4 +32,19 @@ Assert.AreEqual(new Day5().CrateMover9000("../../../Day5/input.test", new Day5()
 Assert.AreEqual(new Day5().CrateMover9000("../../../Day5/input.prod", new Day5().prod), "FWNSHLDNZ");
 
 Assert.AreEqual(new Day5().CrateMover9001("../../../Day5/input.test", new Day5().test), "MCD");
-Assert.AreEqual(new Day5().CrateMover9001("../../../Day5/input.prod", new Day5().prod), "");
+Assert.AreEqual(new Day5().CrateMover9001("../../../Day5/input.prod", new Day5().prod), "RNRGDNFQG");
+
+// -- Day 6 --
+Assert.AreEqual(Day6.FindMarkerString("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+Assert.AreEqual(Day6.FindMarkerString("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+Assert.AreEqual(Day6.FindMarkerString("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+Assert.AreEqual(Day6.FindMarkerString("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+
+Assert.AreEqual(Day6.FindMarker("../../../Day6/input.prod"), 1658);
+
+Assert.AreEqual(Day6.FindMessageString("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+Assert.AreEqual(Day6.FindMessageString("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+Assert.AreEqual(Day6.FindMessageString("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+Assert.AreEqual(Day6.FindMessageString("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+Assert.AreEqual(Day6.FindMessageString("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+Assert.AreEqual(Day6.FindMessage("../../../Day6/input.prod"), 2260);
